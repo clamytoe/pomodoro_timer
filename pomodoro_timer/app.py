3b9814cb40fc79986597c0a5d83e3ec57942d023
Martin Uribe <clamytoe@users.noreply.github.com>
2018-08-01 14:07:22 -0500

    Added logging and refactored the script. Adjusted the tests and disabled logging while testing.

--- pomodoro_timer/app.py
+++ pomodoro_timer/app.py
@@ -24,7 +24,7 @@ class Pomodoro:
     Given the amount of hours you want to spend on your task, it will prompt you to take a 5 minute break every 25
     minutes. At the end it sounds alarm to let you know that the time that you specified is up.
     """
-    MODES = "idle active break".upper().split()
+    MODES = "idle active respite".upper().split()
 
     def __init__(self, duration, breaks=5, interval=25):
         """
@@ -37,69 +37,85 @@ class Pomodoro:
         self.second = 1
         self.minute = self.second * 60
         self.hour = self.minute * 60
-        self.duration = duration * self.hour
-        self.interval = interval * self.minute
-        self.break_length = breaks * self.minute
-        self.start_time = None
-        self.stop_time = None
-        self.break_time = None
-        self.work_time = None
+        self.duration = duration
+        self.breaks = breaks
+        self.interval = interval
+        self.p_duration = self.duration * self.hour
+        self.p_interval = self.interval * self.minute
+        self.p_breaks = self.breaks * self.minute
+        self.session_start = datetime.now()
         self.status = self.MODES[0]
+        self.stop_time = None
         self.rounds = 0
 
+    def __repr__(self):
+        class_name = str(self.__class__).split(".")[-1].replace(">", "")
+        return f"{class_name}(duration:{self.duration} breaks={self.breaks} interval={self.interval})"
+
+    def bye_message(self):
+        logger.info(f"{self.status}, session: end")
+        print("Thanks for using clamytoe's Pomodoro Timer!")
+        exit()
+
     def get_input(self):
+        self.play("warning")
         key = input("Hit any key to continue, [q]uit: ")
         if key.lower() == "q":
             self.bye_message()
 
+        self.play("break") if self.status == "ACTIVE" else self.play("begin")
+
+    def pause(self, length):
+        sleep(length)
+        print("Time for a break!") if self.status == "ACTIVE" else print("Time to get back to work!")
+        self.get_input()
+
     def start(self):
         """Starts the timer."""
-        self.start_time = datetime.now()
-        self.stop_time = self.start_time + timedelta(seconds=self.duration)
-        print(f"Work session ends at: {str(self.stop_time).split('.')[0]}")
+        start_time = datetime.now()
+        self.stop_time = start_time + timedelta(seconds=self.p_duration)
+        logger.info(f"{self.status}, session: start, projected: {self.stop_time}")
+        print(f"Session ends at: {str(self.stop_time).split('.')[0]}")
         self.start_timer()
 
     def start_break(self):
         """Starts the break."""
-        self.break_time = None
-        self.work_time = datetime.now() + timedelta(seconds=self.break_length)
-        print(f"Start again at: {str(self.work_time).split('.')[0]}")
+        work_time = datetime.now() + timedelta(seconds=self.p_breaks)
+        print(f"Start again at: {str(work_time).split('.')[0]}")
         self.status = self.MODES[2]
-        self.play("break")
+        logger.info(f"{self.status}, break_until: {work_time}")
 
     def start_interval(self):
         """Starts the interval timer."""
-        self.work_time = None
-        self.break_time = datetime.now() + timedelta(seconds=self.interval)
-        print(f"Next break: {str(self.break_time).split('.')[0]}")
-        self.status = self.MODES[1]
+        break_time = datetime.now() + timedelta(seconds=self.p_interval)
+        print(f"Next break at: {str(break_time).split('.')[0]}")
         self.rounds += 1
-        self.play("begin")
+        if self.status == "IDLE":
+            self.play("begin")
+        self.status = self.MODES[1]
+        logger.info(f"{self.status}, interval: {self.rounds}")
 
     def start_timer(self):
         try:
             while datetime.now() < self.stop_time:
                 if self.rounds == 4:
-                    print("Nicely done! Go take an extended break.")
+                    print("Nicely done! Go chillax for a bit.")
                     self.play("done")
-                    self.bye_message()
+                    self.rounds = 0
+                    self.status = self.MODES[0]
+                    logger.info(f"{self.status}, intervals: completed")
+                    self.get_input()
                 else:
                     self.start_interval()
-                    self.pause(self.interval)
-                    self.get_input()
+                    self.pause(self.p_interval)
                     self.start_break()
-                    self.pause(self.break_length)
-                    self.get_input()
+                    self.pause(self.p_breaks)
             print("Nicely done! You're all done!.")
             self.play("done")
             self.bye_message()
         except KeyboardInterrupt:
+            logger.info("User terminated session.")
             print("Timer stopped by the user.")
-
-    @staticmethod
-    def bye_message():
-        print("Thanks for using clamytoe's Pomodoro Timer!")
-        exit()
 
     @staticmethod
     def play(sound):
@@ -109,17 +125,14 @@ class Pomodoro:
         :param sound: str - the path and name of the sound file to play
         :return: None
         """
+        logger.info(f"sound: {PLAYER} {SOUNDS[sound]}")
         system(f"{PLAYER} {SOUNDS.get(sound, SOUNDS['warning'])}")
-
-    @staticmethod
-    def pause(length):
-        sleep(length)
 
 
 def get_args():
     """Argument parser."""
     parser = argparse.ArgumentParser(description="Pomodoro Productivity Timer")
-    parser.add_argument("duration", type=int, help="How long you going to work for, in hours")
+    parser.add_argument("-d", "--duration", type=int, help="How long you going to work for, in hours", required=False)
     parser.add_argument("-b", "--breaks", type=int, help="How long the breaks should be", required=False)
     parser.add_argument("-i", "--interval", type=int, help="Minutes to work before taking a break", required=False)
     args = parser.parse_args()
@@ -127,22 +140,25 @@ def get_args():
     breaks = args.breaks
     interval = args.interval
     params = Params(duration=duration, breaks=breaks, interval=interval)
+    logger.info(f"Parsed parameters: {params}")
     return params
 
 
 def main():
     """Main entry point of the application."""
     if platform != "linux":
+        logger.warning(f"Attempted to run on unsupported {platform} platform.")
         print(f"Sorry, your platform {platform} is not supported!")
         exit(1)
 
     params = get_args()
 
-    duration = params.duration
+    duration = params.duration if params.duration else 2
     breaks = params.breaks if params.breaks else 5
     interval = params.interval if params.interval else 25
 
     timer = Pomodoro(duration, breaks=breaks, interval=interval)
+    logger.info(f"Initialize Session: {repr(timer)}")
     timer.start()
 
 
--- tests/test_pomodoro_timer.py
+++ tests/test_pomodoro_timer.py
@@ -3,8 +3,11 @@ test_pomodoro_timer.py
 
 Tests for pomodoro_timer.
 """
+import logging
 import pytest
 from pomodoro_timer import app
+
+logging.disable(logging.CRITICAL)
 
 
 @pytest.fixture
@@ -34,34 +37,26 @@ def test_help():
 
 
 def test_class_initialization(timer):
-    assert timer.start_time is None
-    assert timer.stop_time is None
-    assert timer.duration == 3600
-    assert timer.break_length == 60
-    assert timer.interval == 120
+    assert timer.p_duration == 3600
+    assert timer.p_breaks == 60
+    assert timer.p_interval == 120
     assert timer.status == "IDLE"
     assert timer.rounds == 0
 
 
 def test_start_interval(capfd, timer):
-    assert timer.break_time is None
     timer.start_interval()
     output = capfd.readouterr()[0]
     assert "Next break" in output
-    assert timer.break_time is not None
-    assert timer.work_time is None
     assert timer.status == "ACTIVE"
     assert timer.rounds == 1
 
 
 def test_start_break(capfd, timer):
-    assert timer.work_time is None
     timer.start_break()
     output = capfd.readouterr()[0]
     assert "Start again at" in output
-    assert timer.work_time is not None
-    assert timer.break_time is None
-    assert timer.status == "BREAK"
+    assert timer.status == "RESPITE"
 
 
 def test_done_alarm(timer):
