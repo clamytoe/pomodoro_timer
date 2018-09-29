9a69752482a0d9d47434669a6dcfc1d12c77a71f
Martin Uribe <clamytoe@gmail.com>
2018-09-28 23:23:09 -0500

    Ran black against the script

--- pomodoro_timer/__init__.py
+++ pomodoro_timer/__init__.py
@@ -1,5 +1,5 @@
 from pomodoro_timer import *
 
-__author__: str = 'Martin Uribe'
-__email__: str = 'clamytoe@gmail.com'
-__version__: str = '0.2.0'
+__author__: str = "Martin Uribe"
+__email__: str = "clamytoe@gmail.com"
+__version__: str = "0.2.1"
